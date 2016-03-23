c78c2a0cd510328467882a34a7cc91c541b26169
HansiMou <mhsasd@126.com>
2016-03-23 00:01:45 -0400

    Added example files.

--- src/PageRank.java
+++ src/PageRank.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author Hansi Mou
  * @date Mar 22, 2016
@@ -7,8 +12,42 @@
 /**
  * @author Hansi Mou
  *
- * Mar 22, 2016
+ *         Mar 22, 2016
  */
 public class PageRank {
 
+	/**
+	 * Description:
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int wordCount(String fileName) {
+		File file = new File(fileName);
+		BufferedReader reader = null;
+		int res = 0;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String tempString = null;
+			while ((tempString = reader.readLine()) != null) {
+				String s[] = tempString.split("\\p{Punct}");
+				res += s.length;
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e1) {
+				}
+			}
+		}
+		return res;
+	}
 }
